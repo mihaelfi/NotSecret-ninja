48c0cc207a6eec04fd9f52a279248c0708d2ec97
Michael Fildstein Id:309161594 <Michael Fildstein Id:309161594@monster>
2015-01-13 21:24:05 +0200

    First Commit

---
+++ Client/include/HttpProtocol.h
@@ -0,0 +1,17 @@
+/*
+ * HttpProtocol.h
+ *
+ *  Created on: Jan 13, 2015
+ *      Author: Michael Fildstein Id:309161594
+ */
+
+#ifndef HTTPPROTOCOL_H_
+#define HTTPPROTOCOL_H_
+
+class HttpProtocol {
+public:
+	HttpProtocol();
+	virtual ~HttpProtocol();
+};
+
+#endif /* HTTPPROTOCOL_H_ */
---
+++ Client/src/HttpProtocol.cpp
@@ -0,0 +1,20 @@
+/*
+ * HttpProtocol.cpp
+ *
+ *  Created on: Jan 13, 2015
+ *      Author: Michael Fildstein Id:309161594
+ */
+
+#include "HttpProtocol.h"
+
+HttpProtocol::HttpProtocol() {
+	// TODO Auto-generated constructor stub
+
+}
+
+
+
+HttpProtocol::~HttpProtocol() {
+	// TODO Auto-generated destructor stub
+}
+
