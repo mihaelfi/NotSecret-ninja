3e8863740969795ea0885cd083df214215092a31
Michael Fildstein Id:309161594 <Michael Fildstein Id:309161594@monster>
2015-01-14 20:22:29 +0200

    Done Http GET and POST wrapers.

--- Client/include/HttpProtocol.h
+++ Client/include/HttpProtocol.h
@@ -5,13 +5,24 @@
  *      Author: Michael Fildstein Id:309161594
  */
 
+#include <string>
+
 #ifndef HTTPPROTOCOL_H_
 #define HTTPPROTOCOL_H_
+
+using namespace std;
 
 class HttpProtocol {
 public:
 	HttpProtocol();
+	string  httpPostWrap(string requestURI ,string headers , string messageBody  );
+	string 	httpGetWrap(string requestURI ,string headers);
 	virtual ~HttpProtocol();
+private:
+	string _HTTPVersion = 		"HTTP/1.1";
+	string _requestTypePost = 	"POST" ;
+	string _requestTypeGet = 	"GET";
+
 };
 
 #endif /* HTTPPROTOCOL_H_ */
--- Client/src/HttpProtocol.cpp
+++ Client/src/HttpProtocol.cpp
@@ -7,9 +7,43 @@
 
 #include "HttpProtocol.h"
 
-HttpProtocol::HttpProtocol() {
-	// TODO Auto-generated constructor stub
+#include<string>
 
+using namespace std;
+
+
+
+HttpProtocol::HttpProtocol() {
+
+
+
+}
+
+//<RequestType> <RequestURI> <HTTPVersion>
+//<HeaderName 1 >: <HeaderValue 1>
+//...
+//<HeaderName n >: <HeaderValue n>
+//<MessageBody>
+//$
+
+//string httpPostWrap = requestType + " " + requestURI + " " + this. " \n ";
+//	return httpPostWrap;
+
+
+//POST /login.jps HTTP/1.1
+//
+//UserName=AldoRaine&Phone=495558298
+//$
+
+string HttpProtocol::httpPostWrap(string requestURI ,string headers , string messageBody  ){
+	string httpPostWrap = this->_requestTypePost + " " + requestURI + " " + this->_HTTPVersion + " " + "\n" + headers + "\n" + messageBody + "\n$";
+	return httpPostWrap;
+}
+
+string HttpProtocol::httpGetWrap(string requestURI ,string headers){
+	string httpGetWrap = this->_requestTypeGet + " " + requestURI + " "
+			+ this->_HTTPVersion + " " + "\n" + headers + "\n" + "\n$";
+	return httpGetWrap;
 }
 
 
