d21097b5d1dcadee2e06ea50f659667c2bfa14b8
Michael Fildstein Id:309161594 <Michael Fildstein Id:309161594@monster>
2015-01-14 20:24:11 +0200

    Added 'Client' class for testing.

---
+++ Client/include/Client.h
@@ -0,0 +1,17 @@
+/*
+ * Client.h
+ *
+ *  Created on: Jan 14, 2015
+ *      Author: Michael Fildstein Id:309161594
+ */
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+class Client {
+public:
+	Client();
+	virtual ~Client();
+};
+
+#endif /* CLIENT_H_ */
---
+++ Client/src/Client.cpp
@@ -0,0 +1,50 @@
+/*
+ * Client.cpp
+ *
+ *  Created on: Jan 14, 2015
+ *      Author: Michael Fildstein Id:309161594
+ */
+
+#include <Client.h>
+#include "HttpProtocol.h"
+#include <string>
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+//string HttpProtocol::HttpPostWrap(string requestType , string requestURI ,string headers , string messageBody  ){
+
+int main (){
+	string requestType = "POST";
+	string requestURI  = "login.jsd";
+	string headers	   = "";
+	string messageBody = "userName=Michael&Phone=0545989794";
+
+
+
+	HttpProtocol* protocol = new HttpProtocol();
+
+	cout << protocol->httpPostWrap(requestURI,headers,messageBody);
+	cout << "\n\n\n";
+
+//	string requestType = "POST";
+	requestURI  = "Queue.jsp";
+	headers	   = "Cookie: user_auth= 9e234af900534eff";
+	messageBody = "userName=Michael&Phone=0545989794";
+
+	cout << protocol->httpGetWrap(requestURI,headers);
+
+	delete protocol;
+
+}
+
+Client::Client() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Client::~Client() {
+	// TODO Auto-generated destructor stub
+}
+
